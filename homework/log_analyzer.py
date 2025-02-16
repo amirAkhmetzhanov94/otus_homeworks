cb34134966ec36a980e7d3cb0914d867a83a5d6a
amirAkhmetzhanov94 <amir.akhmetzhanov94@gmail.com>
2025-02-16 09:27:07 +0500

    initial commit

---
+++ homework/log_analyzer.py
@@ -0,0 +1,133 @@
+import os
+import re
+from statistics import median
+from typing import TextIO, Optional, Tuple, List, Dict, Union
+
+# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+config = {
+    "REPORT_SIZE": 1000,
+    "REPORT_DIR": "./reports",
+    "LOG_DIR": "./log"
+}
+
+
+def generate_search_pattern() -> re.Pattern:
+    return re.compile(r'\"([A-Z]+) (/[^\s]+) HTTP/1\.[01]\"\s.*\s(\d+\.\d+)$')
+
+
+def extract_log_data(log_line: str, regex_pattern: re.Pattern) -> Optional[Tuple[str, str]]:
+    match = re.search(regex_pattern, log_line)
+    if match:
+        return match.group(2), match.group(3)
+
+
+def parse_log_file_dates(log_folder: str) -> List[str]:
+    return [file_name.split('-')[-1] for file_name in os.listdir(log_folder)]
+
+
+def get_latest_log_file(log_folder: str) -> str:
+    log_file_dates = parse_log_file_dates(log_folder=log_folder)
+    latest_date = max(log_file_dates)
+    return f'{log_folder}/nginx-access-ui.log-{latest_date}'
+
+
+def count_percentile(number_of_requests: float, total_count: float) -> float:
+    return (number_of_requests / total_count) * 100
+
+
+def count_total(requests_data: dict, key: str) -> float:
+    count_array = []
+    for request in requests_data.values():
+        if isinstance(request[key], list):
+            count_array.append(sum(request[key]))
+            continue
+        count_array.append(request[key])
+    return sum(count_array)
+
+
+def generate_percentiles_report(requests_data: dict, key_to_count: str, total_count: float) -> Dict[str, float]:
+    percentiles = {}
+    for url, data in requests_data.items():
+        if isinstance(data[key_to_count], list):
+            percentile = count_percentile(sum(data[key_to_count]), total_count)
+            percentiles[url] = percentile
+            continue
+        percentile = count_percentile(data[key_to_count], total_count)
+        percentiles[url] = percentile
+    return percentiles
+
+
+def collect_request_data(log_data: list) -> dict:
+    request_data = {}
+    for data in log_data:
+        if data:
+            url, request_time = data
+            if url in request_data:
+                request_data[url]['count'] += 1
+                request_data[url]['times'].append(float(request_time))
+                continue
+            request_data[url] = {'count': 1, 'times': [float(request_time)]}
+    return request_data
+
+
+def count_time_statistics(request_time_array: dict) -> dict:
+    time_stats = {}
+    for url, data in request_time_array.items():
+        time_stats[url] = {
+            'sum': sum(data['times']),
+            'avg': sum(data['times']) / len(data['times']),
+            'max': max(data['times']),
+            'med': median(data['times'])
+        }
+    return time_stats
+
+
+def generate_report_file(
+    time_stats: dict,
+    requests_data: dict,
+    time_percentiles: Dict[str, float],
+    count_percentiles: Dict[str, float]
+):
+    reports = []
+    for url, data in requests_data.items():
+        report = {
+            'count': data['count'],
+            'url': url,
+            'count_perc': float(f'{count_percentiles.get(url):.10f}'),
+            'time_perc': float(f'{time_percentiles.get(url):.10f}'),
+            'time_sum': float(f'{time_stats[url]["sum"]:.10f}'),
+            'time_avg':  float(f'{time_stats[url]["avg"]:.10f}'),
+            'time_med':  float(f'{time_stats[url]["med"]:.10f}'),
+            'time_max': float(f'{time_stats[url]["max"]:.10f}')
+        }
+        reports.append(report)
+    return reports
+
+
+def main():
+    latest_log_file_name = get_latest_log_file(config.get('LOG_DIR'))
+    search_pattern = generate_search_pattern()
+
+    with open(latest_log_file_name, 'r') as log_file:
+        log_data = [extract_log_data(line, search_pattern) for line in log_file.readlines()]
+        requests_data = collect_request_data(log_data)
+
+        requests_total_count = count_total(requests_data, 'count')
+        requests_total_times_count = count_total(requests_data, 'times')
+
+        requests_count_percentiles = generate_percentiles_report(requests_data, 'count', requests_total_count)
+        requests_time_percentiles = generate_percentiles_report(requests_data, 'times', requests_total_times_count)
+
+        time_statistics = count_time_statistics(requests_data)
+
+        reports = generate_report_file(time_statistics, requests_data, requests_count_percentiles, requests_time_percentiles)
+
+        return reports
+
+
+if __name__ == "__main__":
+    main()
