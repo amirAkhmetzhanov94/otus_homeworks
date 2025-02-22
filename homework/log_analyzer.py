3fbfda06affdf17c3f51897e81c1d80f7f36d502
amirAkhmetzhanov94 <amir.akhmetzhanov94@gmail.com>
2025-02-22 19:15:59 +0500

    added try/except on main

--- homework/log_analyzer.py
+++ homework/log_analyzer.py
@@ -271,4 +271,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except SystemExit:
+        raise
+    except BaseException as e:
+        logger.error("Unexpected error occurred", exc_info=True)
