330eb2b9f58903d0cb5ecd9e70966647e63834a5
amirAkhmetzhanov94 <amir.akhmetzhanov94@gmail.com>
2025-03-21 20:50:00 +0500

    added homework_2, completed basic part

---
+++ homework_2_type_annotations/basic/any.py
@@ -0,0 +1,10 @@
+"""
+TODO:
+
+Modify `foo` so it takes an argument of arbitrary type.
+"""
+from typing import Any
+
+
+def foo(some_argument: Any):
+    """⬆️ Change me. No need to implement the function."""
---
+++ homework_2_type_annotations/basic/final.py
@@ -0,0 +1,8 @@
+"""
+TODO:
+
+Make sure `my_list` cannot be re-assigned to.
+"""
+from typing import Final, List
+
+my_list: Final[List[int]] = []
